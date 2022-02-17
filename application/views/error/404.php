ef770ec9bf88e9bfa1a711eaecdab15fdea6cf03
zaqueu <zaqueu.alves01@gmail.com>
2022-02-16 21:45:33 -0300

    ajuste da view da página de 404

--- application/views/error/404.php
+++ application/views/error/404.php
@@ -1,7 +1,6 @@
 <div id="page-error">
     <div class="center">
-            <div class="item"><img src="assets/imgs/error404.png" width="30%"></div>
-            <div class="item">Página não encontrada.</div>            
-        </span>
+        <div class="item"><img src="assets/imgs/error404.png" width="30%"></div>
+        <div class="item">Página não encontrada.</div>
     </div>
 </div>
