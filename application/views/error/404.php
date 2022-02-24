e1ae6ada05a9c98620342825bc33e7e180fcf1cf
zaqueu <zaqueu.alves01@gmail.com>
2022-02-24 20:32:50 -0300

    2022-02-04

--- application/views/error/404.php
+++ application/views/error/404.php
@@ -1,8 +1,8 @@
 <div id="page-error">
     <div class="center">
-        <div class="item">
-            <img src="assets/imgs/error404.png" width="30%">
-        </div>
-        <div class="item">Página não encontrada.</div>
+
+        <img src="assets/imgs/error404.png" width="30%">
+        <p>Página não encontrada.</p>
+
     </div>
 </div>
