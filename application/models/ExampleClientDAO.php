687f9896feaa317f7d08607753ba95aca3e0c674
Zaqueu Alves <69696525+zaqueu-git@users.noreply.github.com>
2022-05-12 08:24:11 -0300

    Update ExampleClientDAO.php

--- application/models/ExampleClientDAO.php
+++ application/models/ExampleClientDAO.php
@@ -6,21 +6,16 @@ use application\helpers\DataAccessObject;
 
 class ExampleClientDAO extends Model
 {
+    private $conn;
+    
+    public function __construct()
+    {
+        this->conn = new DataAccessObject($this->systemConn);
+    }
+    
     public function create()
     {
-        $DataAccessObject = new DataAccessObject($this->systemConn);
-
-        //$rs = $DataAccessObject->select("SELECT * FROM people");
-
-        var_dump($this->systemConn);
-
-        /*
-        $data = $conn->query('SELECT * FROM minhaTabela WHERE nome = ' . $conn->quote($name));
-
-        foreach($data as $row) {
-            print_r($row);
-        }
-        */
-
+        $rs = $conn->select("SELECT * FROM people");
+        var_dump($rs);
     }
 }
